e730b7df96c94bd24755883478f625486a909938
anion0278 <anion0278@gmail.com>
2020-04-22 18:22:42 +0200

    Added global keyboard hooks (F2+F4) and keyboard emulator for auto-filling the tracking number and current date Tracking number is stored in settings Tracking number can be changed in new Tab

---
+++ WindowHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Martin_app
+{
+    public static class WindowHelper
+    {
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+        public static string GetActiveWindowTitle()
+        {
+            const int nChars = 256;
+            var Buff = new StringBuilder(nChars);
+            IntPtr handle = GetForegroundWindow();
+
+            if (GetWindowText(handle, Buff, nChars) > 0)
+            {
+                return Buff.ToString();
+            }
+            return null;
+        }
+    }
+}
