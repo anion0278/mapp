dccd203cd056203952161f19efa6ac0a1257785c
anion0278 <anion0278@gmail.com>
2020-12-23 21:53:37 +0100

    Reading available transaction configs from JSON files Requires separation to layers Replaced Newton JSON nuget with Core nuget

---
+++ MarketPlaceTransactionsConfig.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Globalization;
+using System.Text.Json.Serialization;
+
+namespace Martin_app
+{
+    public class MarketPlaceTransactionsConfig 
+    {
+        public MarketPlaceTransactionsConfig(string dateCultureInfoName, string dateSubstring, int marketPlaceId)
+        {
+            DateCultureInfoName = dateCultureInfoName;
+            DateSubstring = dateSubstring;
+            MarketPlaceId = marketPlaceId;
+        }
+
+        /// <summary>
+        /// Number of lines before the column-name-line.
+        /// </summary>
+        public int LinesToSkipBeforeColumnNames { get; set; }
+
+        public string DateCultureInfoName { get; }
+
+        private string _timeSeparatorOverride = null;
+        public string TimeSeparatorOverride
+        {
+            get { return _timeSeparatorOverride; }
+            set
+            {
+                if (_dataCultureInfo != null)
+                {
+                    _dataCultureInfo.DateTimeFormat.TimeSeparator = TimeSeparatorOverride;
+                }
+
+                _timeSeparatorOverride = value;
+            }
+        }
+
+        private CultureInfo _dataCultureInfo;
+
+        public CultureInfo DateCultureInfo
+        {
+            get
+            {
+                if (_dataCultureInfo == null)
+                {
+                    _dataCultureInfo = new CultureInfo(DateCultureInfoName);
+                    if (_timeSeparatorOverride != null)
+                    {
+                        _dataCultureInfo.DateTimeFormat.TimeSeparator = _timeSeparatorOverride;
+                    }    
+                }
+                return _dataCultureInfo;
+            }
+        }
+
+        // it is decided to  use this phrase because parameter Market place can be unavailable (no transactions)
+        public string DistinctionPhrase { get; set; }
+
+        public string DateSubstring { get; }
+
+        public string OrderIdColumnName { get; set; }
+
+        public int MarketPlaceId { get; set; }
+
+        public string TransactionTypeColumnName { get; set; }
+
+        public IEnumerable<string> DateTimeColumnNames { get; set; } // TODO should be of type TransactionParameter: columnNames, ParseType(enum) 
+        public IEnumerable<string> ValueComponentsColumnName { get; set; }
+
+        public IEnumerable<string> TransferTypeNames { get; set; }
+        public IEnumerable<string> RefundTypeNames { get; set; }
+        public IEnumerable<string> OrderTypeNames { get; set; }
+        public IEnumerable<string> ServiceFeeTypeNames { get; set; }
+        public string TotalPriceColumnName { get; set; }
+    }
+
+
+    public class MarketPlaceTransactionsConfigDTO
+    {
+        protected MarketPlaceTransactionsConfigDTO()
+        {}
+
+        public int LinesToSkipBeforeColumnNames { get; set; }
+
+        public int MarketPlaceId { get; set; }
+        
+        public string DistinctionPhrase { get; set; }
+
+        public string DateSubstring { get; set; }
+
+        public string TimeSeparatorOverride { get; set; }
+
+        public string DateCultureInfoName { get; set; }
+
+        public string OrderIdColumnName { get; set; }
+
+        public string TransactionTypeColumnName { get; set; }
+
+        public IEnumerable<string> DateTimeColumnNames { get; set; } 
+        public IEnumerable<string> ValueComponentsColumnName { get; set; }
+
+        public IEnumerable<string> TransferTypeNames { get; set; }
+        public IEnumerable<string> RefundTypeNames { get; set; }
+        public IEnumerable<string> OrderTypeNames { get; set; }
+        public IEnumerable<string> ServiceFeeTypeNames { get; set; }
+        public string TotalPriceColumnName { get; set; }
+    }
+}
--- Transaction.cs
+++ Transaction.cs
@@ -19,16 +19,10 @@ namespace Martin_app
 
         public TransactionTypes Type { get; set; }
 
-        public AmazonMarketplace Marketplace { get; set; }
+        public int MarketplaceId { get; set; }
 
         public string OrderId { get; set; }
 
-        public string ProductDescription { get; set; }
-
-
-        // TODO rename, its not price but TOTAL of transaction
-        // in GPC only the price for item, shipping, sale(rebate) is relevant
-        // in JP case also some additional Fees
         public double TransactionValue { get; set; }
     }
 }
