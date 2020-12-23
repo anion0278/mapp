e88e9ff997d93d9a6cab6c84c27de9a8fb346cfa
anion0278 <anion0278@gmail.com>
2020-12-23 18:32:19 +0100

    Added conversion of Paypal summary to GPC

--- Transaction.cs
+++ Transaction.cs
@@ -25,19 +25,10 @@ namespace Martin_app
 
         public string ProductDescription { get; set; }
 
-        public int Quantity { get; set; }
-
-        public double ProductPrice { get; set; }
-
-        public double PromotionRebate { get; set; }
-
-        public IEnumerable<double> AdditionalFees { get; set; } 
-
-        public double ShippingPrice { get; set; }
 
         // TODO rename, its not price but TOTAL of transaction
         // in GPC only the price for item, shipping, sale(rebate) is relevant
         // in JP case also some additional Fees
-        public double TotalPrice => ProductPrice + ShippingPrice + PromotionRebate + AdditionalFees.Sum();
+        public double TransactionValue { get; set; }
     }
 }
