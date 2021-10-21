4629bfaa644a1596d818c0753e6b3abeba787f1c
anion0278 <anion0278@gmail.com>
2021-10-21 21:15:14 +0200

    feature: App Center integration

--- Mapp.UI/App.xaml.cs
+++ Mapp.UI/App.xaml.cs
@@ -9,6 +9,9 @@ using System.Threading.Tasks;
 using System.Windows;
 using Shmap.ViewModels;
 using Shmap.Views;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 using NLog;
 
 namespace Mapp
@@ -31,7 +34,7 @@ namespace Mapp
 
         public override void WriteLine(string message)
         {
-            BindingErrorEventHandler.Invoke(this, new BindingException(message));
+            BindingErrorEventHandler?.Invoke(this, new BindingException(message));
             Flush();
         }
     }
@@ -43,15 +46,16 @@ namespace Mapp
 
         protected override void OnStartup(StartupEventArgs e)
         {
+            base.OnStartup(e);
+            AppCenter.Start("9549dd3a-1371-4a23-b973-f5e80154119d", typeof(Analytics), typeof(Crashes)); // TODO should solve secrt storing somehow :(
+            SetupExceptionHandling();
+
             PresentationTraceSources.Refresh();
             var wpfBindingErrorHandler = new ExplicitBindingErrorTraceListener();
             wpfBindingErrorHandler.BindingErrorEventHandler += (o, e) => LogException(e, "WPF Binding");
             PresentationTraceSources.DataBindingSource.Listeners.Add(wpfBindingErrorHandler);
             PresentationTraceSources.DataBindingSource.Switch.Level = SourceLevels.Error;
             PresentationTraceSources.FreezableSource.Switch.Level = SourceLevels.Off; // needed in order to hide exceptions from Freezables
-
-            base.OnStartup(e);
-            SetupExceptionHandling();
 
             var mainWindow = new MainWindow();
             Current.MainWindow = mainWindow;
@@ -82,6 +86,7 @@ namespace Mapp
             string message = $"Unhandled exception with source: {source}. ";
             try
             {
+                Crashes.TrackError(exception);
                 var assemblyName = Assembly.GetExecutingAssembly().GetName();
                 message += $"Unhandled exception in {assemblyName.Name} v{assemblyName.Version}";
                 message += $"\n * Top-most message: {exception.Message} \n * stack: {exception.StackTrace}";
