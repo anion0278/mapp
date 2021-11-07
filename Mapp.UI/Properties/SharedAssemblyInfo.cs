5f51da94e4ae66c1b86ca7f3971065d48cb1023b
AutomatedRelease <automatedRelease@noemail.com>
2021-11-07 21:34:39 +0000

    chore: Automated report [skip ci]

--- Mapp.UI/Properties/SharedAssemblyInfo.cs
+++ Mapp.UI/Properties/SharedAssemblyInfo.cs
@@ -22,5 +22,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("2767955e-07e8-410d-83d2-8a27cf1d63d9")]
 
-[assembly: AssemblyVersion("3.1.0.4")]
-[assembly: AssemblyFileVersion("3.1.0.4")]
+[assembly: AssemblyVersion("3.2.0.5")]
+[assembly: AssemblyFileVersion("3.2.0.5")]
